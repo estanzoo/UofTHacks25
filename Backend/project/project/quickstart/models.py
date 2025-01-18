62795662a1d7e49a21f94e6ed3ca3f0dd45284a0
estanzoo <mysupppermail@gmail.com>
2025-01-18 01:28:19 -0500

    Basic User model added in Models.py

--- Backend/project/project/quickstart/models.py
+++ Backend/project/project/quickstart/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User (AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255)
+    password = models.CharField(blank=True, max_length=255)
