7687a379486d038916a1c0cc2242b71f2a76830d
alexypeng <81698776+alexypeng@users.noreply.github.com>
2025-01-18 18:34:30 -0500

    Add errors to views.py

--- Backend/project/project/views.py
+++ Backend/project/project/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import User, Group
 from .serializers import UserSerializer, GroupSerializer
 from rest_framework.decorators import api_view
@@ -18,11 +18,12 @@ def user_list(request):
     
     if request.method == 'POST':
         serializer = UserSerializer (data=request.data, many = True)
+        print("Get me a user list")
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
-            return Response (status = status.HTTP_400_BAD_REQUEST)
+            return Response(status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST'])
 def group_list(request):
@@ -38,15 +39,16 @@ def group_list(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
-            return Response (status = status.HTTP_400_BAD_REQUEST)
+           return Response (status = status.HTTP_400_BAD_REQUEST)
         
 @api_view(['ADD'])
 def add_user_to_group(user_id, group_id):
     try:
         group = Group.objects.get(id=group_id)
+        print("adding user...")
 
         if group.members.contains(id=user_id):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+           return Response(status=status.HTTP_400_BAD_REQUEST)
         
         group.members.add(id=user_id)
         return Response(status=status.HTTP_200_OK)
@@ -69,3 +71,8 @@ def remove_user_from_group(user_id, group_id):
         return Response(status=status.HTTP_404_NOT_FOUND)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+def query_view(request):
+    query = request.GET.get('query', 'No query provided')
+    return HttpResponse(f"Received query: {query}")
+
