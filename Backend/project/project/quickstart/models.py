8c8b838cc2d36f974a96ba665538fbc975b7bd7a
estanzoo <mysupppermail@gmail.com>
2025-01-18 01:40:56 -0500

    Updated user

--- Backend/project/project/quickstart/models.py
+++ Backend/project/project/quickstart/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 # Create your models here.
 class User (AbstractUser):
-    username = models.CharField(max_length=255, unique=True)
-    full_name = models.CharField(max_length=255)
-    password = models.CharField(blank=True, max_length=255)
+    display_name = models.CharField(max_length=255, unique=False)
+    user_id = models.CharField(max_length=255, unique=True)
--- Backend/project/project/quickstart/views.py
+++ Backend/project/project/quickstart/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from .models import User
 
 # Create your views here.
