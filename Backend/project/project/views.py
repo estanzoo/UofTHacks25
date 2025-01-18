a3876f71d8134adbb0d39647ac47cfc9c3cccfb2
estanzoo <mysupppermail@gmail.com>
2025-01-18 12:11:33 -0500

    added POST for users

--- Backend/project/project/views.py
+++ Backend/project/project/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import User, Group
 from .serializers import UserSerializer, GroupSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 def user_list(request):
     users = User.objects.all()
     serializer = UserSerializer (users, many=True)
-    return JsonResponse (serializer.data)
+    return JsonResponse ({'users':serializer.data})
 
+@api_view(['GET', 'POST'])
 def group_list(request):
-    groups = Group.objects.all()
-    serializer = GroupSerializer (data=groups, many=True)
-    print(serializer.is_valid())
-    return JsonResponse (serializer.data, safe = False)
+    if request.method == 'GET':
+        groups = Group.objects.all()
+        serializer = GroupSerializer (groups, many=True)
+        return JsonResponse ({'groups': serializer.data})
+    
+    if request.method == 'POST':
+        serializer = GroupSerializer (data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
