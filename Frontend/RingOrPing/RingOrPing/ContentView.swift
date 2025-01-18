2a3d64311d831c224340983d9a4311f64d22a00e
Daniel Ye <danielyeqt@gmail.com>
2025-01-18 11:50:05 -0500

    Add Username

--- Frontend/RingOrPing/RingOrPing/ContentView.swift
+++ Frontend/RingOrPing/RingOrPing/ContentView.swift
@@ -9,20 +9,43 @@ import SwiftUI
 import UIKit
 
 let bgColor: Color = Color("Dark")
+let topBarColor: Color = Color("DarkChange")
+
+struct TopBarView: View {
+    @State var displayName: String = ""
+    var body: some View {
+        VStack() {
+            Spacer().frame(height: 40)
+            ZStack() {
+                TextField("Display Name", text:$displayName, prompt: Text("Display Name").foregroundStyle(Color("ExtraDark")))
+                    .padding()
+                    .font(.system(size: 36, weight: .bold))
+                    .padding(.horizontal)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .padding()
+        .background(topBarColor)
+        .frame(width: .infinity, height: 150)
+    }
+}
 
 struct ContentView: View {
+    let topBarView = TopBarView()
+    
     var body: some View {
         ZStack {
-            bgColor.ignoresSafeArea()
-            
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+            VStack() {
+                topBarView
+                Spacer()
             }
-            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea(.all)
+        .background(bgColor)
     }
 }
 
