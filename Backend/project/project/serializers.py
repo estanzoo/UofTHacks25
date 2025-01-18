eda7a731fe195e6bd2522552a0b9f3d468efbda2
estanzoo <mysupppermail@gmail.com>
2025-01-18 12:32:01 -0500

    finished template POST methods

--- Backend/project/project/serializers.py
+++ Backend/project/project/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import User, Group
 
-class UserSerializer (serializers.Serializer):
-    display_name = serializers.CharField (max_length=255)
-    user_id = serializers.CharField (max_length=255)
+class UserSerializer (serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['display_name', 'user_id']
 
-class GroupSerializer (serializers.Serializer):
-    display_name = serializers.CharField (max_length=255)
-    group_id = serializers.CharField (max_length=255)
-    join_code = serializers.CharField (max_length=6)
+class GroupSerializer (serializers.ModelSerializer):
+    class Meta: 
+        model = Group
+        fields = ['display_name', 'group_id', 'join_code']
--- Backend/project/project/views.py
+++ Backend/project/project/views.py
@@ -8,10 +8,21 @@ from rest_framework import status
 
 # Create your views here.
 
+@api_view(['GET', 'POST'])
 def user_list(request):
-    users = User.objects.all()
-    serializer = UserSerializer (users, many=True)
-    return JsonResponse ({'users':serializer.data})
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer (users, many=True)
+        print (serializer.data)  
+        return JsonResponse ({'users':serializer.data})
+    
+    if request.method == 'POST':
+        serializer = UserSerializer (data=request.data, many = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response (status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST'])
 def group_list(request):
@@ -21,7 +32,10 @@ def group_list(request):
         return JsonResponse ({'groups': serializer.data})
     
     if request.method == 'POST':
-        serializer = GroupSerializer (data=request.data)
+        serializer = GroupSerializer (data=request.data, many = True)
+        print (serializer.is_valid())
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response (status = status.HTTP_400_BAD_REQUEST)
