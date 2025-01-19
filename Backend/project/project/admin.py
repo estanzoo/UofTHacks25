170f45834fd0007ff4a620386f713d00abf996bc
estanzoo <mysupppermail@gmail.com>
2025-01-19 00:45:20 -0500

    added admin for grouo

--- Backend/project/project/admin.py
+++ Backend/project/project/admin.py
@@ -1,2 +1,15 @@
 # admin.py
 from django.contrib import admin
+from .models import User, Group
+
+class UserProfileAdmin(admin.ModelAdmin):
+    # Explicitly list fields you want to show in the admin form
+    fields = ['display_name', 'user_id', 'phone_number']  # Ensure phone_number is included
+
+admin.site.register(User, UserProfileAdmin)
+
+class GroupProfileAdmin(admin.ModelAdmin):
+    # Explicitly list fields you want to show in the admin form
+    fields = ['display_name', 'group_id', 'joi_code', 'members']  # Ensure phone_number is included
+
+admin.site.register(Group, GroupProfileAdmin)
